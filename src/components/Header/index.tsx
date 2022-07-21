d30ca943d80f65ff35d5ce30db849a0ca5bcd59a
Antonio <antoniopsilva083@gmail.com>
2022-07-20 23:17:12 -0300

    feat: create navigation between pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer } from './styles'
+import { Scroll, Timer } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logoIgnite} alt="" />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
